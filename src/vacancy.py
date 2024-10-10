00381541526589468fea9ae14d4f826c22216414
Nikita Khramov <nikuser12321@gmail.com>
2024-10-10 22:51:16 +0300

    Added __str__

--- src/vacancy.py
+++ src/vacancy.py
@@ -13,6 +13,13 @@ class Item(ABC):
         pass
 
 
+class Processor(ABC):
+
+    @abstractmethod
+    def add_vacancy(self, *args):
+        pass
+
+
 class Vacancy(Item):
 
     __slots__ = ("name", "link", "pay", "area", "description")
@@ -37,7 +44,34 @@ class Vacancy(Item):
         return json.dumps(self.to_dict())
 
 
+class VacanciesProcessor(Processor):
+
+    __slots__ = "vacancies"
+
+    def __init__(self, vacancies: None | list = None):
+        self.__vacancies = vacancies if vacancies else []
+
+    def add_vacancy(self, *args: Vacancy):
+        for arg in args:
+            if issubclass(arg.__class__, Vacancy):
+                self.__vacancies.append(arg)
+            else:
+                raise TypeError
+
+    def __str__(self):
+        result_list = []
+        for vacancy in self.__vacancies:
+            result_list.append(f"{vacancy.name}: {vacancy.description}\n")
+        return "".join(result_list)
+
+
 if __name__ == "__main__":
-    vacancy = Vacancy("1", "2", "3", "4", "5")
-    print(vacancy.to_json())
-    print(vacancy.to_dict())
+    vacancy_1 = Vacancy("1", "2", "3", "4", "5")
+    vacancy_2 = Vacancy("6", "7", "8", "9", "0")
+    data = VacanciesProcessor()
+    data.add_vacancy(vacancy_1)
+    data.add_vacancy(vacancy_2)
+    print(vacancy_1.to_json())
+    print(vacancy_2.to_dict())
+    print(data)
+
