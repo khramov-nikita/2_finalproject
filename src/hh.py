3231ad7cddc241f4edbc8eb82eb43077cbacbdef
Nikita Khramov <nikuser12321@gmail.com>
2024-10-11 02:16:02 +0300

    Small changes

--- src/hh.py
+++ src/hh.py
@@ -25,23 +25,27 @@ class HH(Parser):
     """
 
     def __init__(self):
-        self.url = "https://api.hh.ru/vacancies"
-        self.headers = {"User-Agent": "HH-User-Agent"}
-        self.params = {"text": "", "page": 0, "per_page": 100}
-        self.vacancies = []
+        self.__url = "https://api.hh.ru/vacancies"
+        self.__headers = {"User-Agent": "HH-User-Agent"}
+        self.__params = {"text": "", "page": 0, "per_page": 100}
+        self.__vacancies = []
         # super().__init__(file_worker)
 
     def load_vacancies(self, keyword):
         """
         Метод для выгрузки вакансий
         """
-        self.params["text"] = keyword
+        self.__params["text"] = keyword
 
-        while self.params.get("page") != 2:
-            response = requests.get(self.url, headers=self.headers, params=self.params)
+        while self.__params.get("page") != 2:
+            response = requests.get(self.__url, headers=self.__headers, params=self.__params)
             vacancies = response.json()["items"]
-            self.vacancies.extend(vacancies)
-            self.params["page"] += 1
+            self.__vacancies.extend(vacancies)
+            self.__params["page"] += 1
+
+    @property
+    def vacancies(self):
+        return self.__vacancies
 
 
 if __name__ == "__main__":
